ebe2417c19600bf7a09595e7ba400db5ac4e9aad
Yan Wang <grapeot@gmail.com>
2017-03-26 10:23:45 -0700

    Initial import.

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Launch the program
+python3 main.py
---
+++ utilities.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Some utility functions
+"""
+import logging
+
+class NickNameLookup:
+    def __init__(self, chatrooms):
+        self.dict = {}
+        for chatroom in chatrooms:
+            for member in chatroom['MemberList']:
+                self.dict[member['UserName']] = member['DisplayName'] if member['DisplayName'] != '' else member['NickName']
+
+    def lookupNickName(self, msg):
+        if 'ActualNickName' in msg and msg['ActualNickName'] != '':
+            return msg['ActualNickName']
+        username = msg['ActualUserName']
+        if username in self.dict:
+            return self.dict[username]
+        else:
+            return '未知昵称'
+
+def extractFromUserName(msg):
+    # It's messy that if the sender is yourself, the group name will appear in the ToUserName
+    if 'ToUserName' in msg and msg['ToUserName'] != '':
+        return msg['ToUserName']
+
+def extractToUserName(msg):
+    # For group chat FromUserName is the group Id
+    if 'FromUserName' in msg and msg['FromUserName'] != '':
+        return msg['FromUserName']
+    elif 'User' in msg and msg['User']['UserName'] != '':
+        return msg['User']['UserName']
+    else:
+        return '未知昵称'
+
+def getChatroomByName(chatrooms, name):
+    groups = [ x for x in chatrooms if x['NickName'] == name ]
+    if len(groups) == 0:
+        logging.error('Cannot find the chatroom named {0}.'.format(name))
+        return None
+    return groups[0]
+
+def getNameForChatroomDisplayName(name):
+    return name.replace('&', '&amp;')
